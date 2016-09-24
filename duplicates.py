5261fdbc96bb89a1d8ec5935d234d87808811fb6
Komarov Valentin <komarovvv36@gmail.com>
2016-09-24 15:05:21 +0300

    First commit

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,49 @@
+import os
+import argparse
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+GLOBAL_DICTIONARY = dict()
+
+
+def get_files(file_path):
+    files_set = set()
+    for root, subdirs, files in os.walk(file_path):
+        if files:
+            files_set.update(
+                {
+                    '{}.{}'.format(
+                        file,
+                        os.stat(os.path.join(root, file)).st_size,
+                    ) for file in files
+                }
+            )
+            for file in files:
+                GLOBAL_DICTIONARY['{}.{}'.format(
+                    file,
+                    os.stat(os.path.join(root, file)).st_size,
+                )] = os.path.join(root, file)
+    return files_set
+
+
+def remove_duplicates(file_path1, file_path2):
+    first_set = get_files(file_path1)
+    second_set = get_files(file_path2)
+    [os.remove(GLOBAL_DICTIONARY[file]) for file in first_set & second_set]
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p1',
+        '--path1',
+        help='Path to first folder',
+        required=True,
+    )
+    parser.add_argument(
+        '-p2',
+        '--path2',
+        help='Path to second folder',
+        required=True,
+    )
+    args = parser.parse_args()
+    remove_duplicates(args.path1, args.path2)
