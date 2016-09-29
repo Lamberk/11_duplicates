be7a487d63f4828844270d8fa87dcec41122200c
Komarov Valentin <komarovvv36@gmail.com>
2016-09-30 01:36:22 +0300

    Refactoring

--- duplicates.py
+++ duplicates.py
@@ -7,21 +7,18 @@ def get_files(file_path):
     files_dict = defaultdict(list)
     for root, subdirs, files in os.walk(file_path):
         if files:
-            for file in files:
+            for single_file in files:
                 files_dict['{}.{}'.format(
-                    file,
-                    os.stat(os.path.join(root, file)).st_size,
-                )].append(os.path.join(root, file))
+                    single_file,
+                    os.stat(os.path.join(root, single_file)).st_size,
+                )].append(os.path.join(root, single_file))
     return files_dict
 
 
 def find_duplicates(file_path):
     files = get_files(file_path)
     dupl_files = [path for path in files.values() if len(path) > 1]
-    if dupl_files:
-        print('Duplicates found: ', dupl_files)
-    else:
-        print('No duplicates found')
+    return dupl_files
 
 
 if __name__ == '__main__':
@@ -33,4 +30,8 @@ if __name__ == '__main__':
         required=True,
     )
     args = parser.parse_args()
-    find_duplicates(args.path)
+    dupl_files = find_duplicates(args.path)
+    if dupl_files:
+        print('Duplicates found: ', dupl_files)
+    else:
+        print('No duplicates found')
